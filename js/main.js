a05d44f58678131b97208ef055eef22039aa9f07
amirjanoff <amirjan001@gmail.com>
2023-01-11 23:02:08 +0500

    complated project

---
+++ js/main.js
@@ -0,0 +1,167 @@
+// var sum = 0;
+// var a = +prompt("son kiriting!");
+// for ( i = 0; i < a; i++) {
+//   sum = sum + i
+
+// }
+// console.log(sum)
+
+// var a = +prompt("son kiriting!");
+// var zero = 0;
+// for (let i = 1; i <= a; i++) {
+
+//   if (i % 2 === 0) zero = zero + i;
+// }
+// console.log(zero);
+
+// const a = +prompt("son kiriting!");
+// let zero = 0;
+
+// for (let i = 0; i <= a; i++) {
+//   if (i % 2 === 1) zero = zero + i;
+// }
+// console.log(zero);
+
+// const a = +prompt("son kiriting!");
+// let zero = 0;
+
+// for (let i = 0; i <= a; i++) {
+//   if (i % 5 === 0) zero = zero + i;
+// }
+
+// console.log(zero);
+
+// const a = 15;
+// let count = 0;
+// for ( let i = 0; i < a; i++) {
+//   if ( i % 2 === 0) count +=1
+// }
+// console.log(count)
+
+// const a = 2;
+// const b = 8;
+// let count = 0;
+// for (let i = a; i < b; i++) {
+//   count = count + i;
+// }
+// console.log(count);
+
+// const a = 4;
+// const b = 15;
+// let count = 0;
+// for (let i = a; i < b; i++) {
+//   if (i % 2 === 0) count +=1;
+// }
+// console.log(count);
+
+// const a = 4;
+// const b = 15;
+// let count = 0;
+// let sum = 0;
+// for (let i = a; i < b; i++) {
+//   if (i % 3 === 0) count += 1
+//   sum = sum + i;
+// }
+// console.log(count, sum);
+
+// const a = 6;
+// const b = 16;
+// let count = 0;
+// for (let i = a; i < b; i++) {
+//   if (i % 2 === 0 && i % 3 === 0) {
+//     count += 1;
+//   }
+// }
+// console.log(count);
+
+// const a = -1;
+// const b = 10;
+// let count = 0;
+// for (let i = a; i <= b; i++) {
+//   if (i > 0) {
+//     count += 1;
+//   }
+// }
+// console.log(count);
+
+// const a = 2;
+// let count = 1;
+// for (let i = count; i <= 5; i++) {
+//   count = count * a;
+// }
+// console.log(count);
+
+// const a = 2;
+// let count = 1;
+// let n = 7;
+// for (let i = 1; i <= n; i++) {
+//   count = count * a;
+// }
+// console.log(count);
+
+// const a = 3;
+// const n = 4;
+// let count = 0;
+// let s = 0;
+// for (let i = 0; i < 4; i++) {
+//   count = count * 10 + a;
+//   s = s + count;
+// }
+// console.log(s);
+
+// const a = 28;
+// let s = 0;
+// for (let i = 1; i < 28; i++) {
+//   if (a % i === 0) {
+//     s = s + i;
+//   }
+//   if (a === s) {
+//     console.log("true");
+//   } else {
+//     console.log("false");
+//   }
+// }
+
+// const a = 6;
+// let count = 0;
+// for (let i = 1; i <= 6; i++) {
+//   count = count + i * i;
+// }
+// console.log(count);
+
+// const number = 256;
+
+// function isArmstrongNumber(number) {
+//   const numberStr = number.toString();
+//   let sum = 0;
+//   for(let i = 0; i < numberStr.length; i++) {
+//     sum += Number(numberStr[i]) ** numberStr.length;
+//   }
+
+//   return sum === number;
+// }
+
+// console.log(isArmstrongNumber(256));
+
+// const num = 13;
+// let count = 0;
+// for (let i = 1; i <= num; i++) {
+//   if (num % i === 0) {
+//    count = count + 1;
+//   }
+
+//   if (count === 2) {
+//     console.log(true);
+//   } else {
+//     console.log(false);
+//   }
+// }
+
+// const a = 6;
+// let count = 1;
+// for (let i = 1; i <= a; i++) {
+//   count = count * i;
+// }
+// console.log(count);
+
+
